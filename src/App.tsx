178f9121276fe9fd471516775be2c8b7f1f45732
Luiz Gonzaga Euflauzino Da Silva <150976494+Gonzag4@users.noreply.github.com>
2026-05-10 06:08:21 -0300

    vizualizar perfil paciente

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login.tsx';
 import DeletarConta from './pages/DeletarConta'
+import VisualizarPerfilPaciente from './pages/VisualizarPerfilPaciente'
 
 function App() {
   return (
     <Routes>
       <Route path='/login' element={<Login />} />
-        <Route path="/conta/excluir" element={<DeletarConta />} />
+      <Route path="/conta/excluir" element={<DeletarConta />} />
+
+      {/* UC06 - Visualizar cadastro de paciente */}
+      <Route path='/perfil/paciente' element={<VisualizarPerfilPaciente />} />
     </Routes>
   )
 }
