2ddf1a6b88b681413d0c8b341b05433216db3df5
danilodepadua <pwa.danilo@gmail.com>
2026-05-16 12:31:27 -0300

    exclusão de arquivots d.ts

--- src/components/ModalDeletarConta.d.ts
+++
@@ -1,9 +0,0 @@
-interface ModalDeletarContaProps {
-    isOpen: boolean;
-    onClose: () => void;
-    onConfirm: (senha: string) => Promise<void>;
-    tipoUsuario: 'paciente' | 'profissional';
-    temConsultasAbertas: boolean;
-}
-export default function ModalDeletarConta({ isOpen, onClose, onConfirm, tipoUsuario, temConsultasAbertas }: ModalDeletarContaProps): import("react/jsx-runtime").JSX.Element | null;
-export {};
--- src/components/Sidebar.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-interface SidebarProps {
-    role: UserRole;
-    itemAtivo: string;
-    atendimentoAtivo?: boolean;
-}
-type UserRole = 'paciente' | 'profissional' | 'administrador';
-declare const Sidebar: React.FC<SidebarProps>;
-export default Sidebar;
