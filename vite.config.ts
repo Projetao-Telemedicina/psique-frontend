0c6191397f94ab220cca16b2031515f04959c219
Láila Lemos <samaralailaa@gmail.com>
2026-05-27 23:56:05 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
-        //target: 'https://psique-backend.onrender.com',
+        //target: 'http://localhost:3000',
+        target: 'https://psique-backend.onrender.com',
         changeOrigin: true,
-        //secure: true,
-        secure: false,
+        secure: true,
+        //secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
