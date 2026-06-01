a3b63322fb0c1215be3a85994c6b210907070f9b
danilodepadua <101139028+danilodepadua@users.noreply.github.com>
2026-06-01 03:38:25 -0300

    Revert "Feat/uc8 botao online"

--- src/pages/TelaInicialCliente.tsx
+++ src/pages/TelaInicialCliente.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
 import EmergencyButton from '../components/EmergencyButton';
-import { EmergencyModal } from "../components/EmergencyModal";
 import { useAuth } from '../components/AuthContext';
 import {MatchModal} from "../components/MatchModal";
 
@@ -54,7 +53,6 @@ interface AppointmentData {
 export default function TelaInicialPaciente() {
     const [showMatchModal, setShowMatchModal] = useState(false);
     const navigate = useNavigate();
-    const [showEmergencyModal, setShowEmergencyModal] = useState(false);
     const { user, token } = useAuth();
 
     const [nomeUsuario, setNomeUsuario] = useState('Paciente');
@@ -163,11 +161,6 @@ export default function TelaInicialPaciente() {
                 role="PATIENT"
             />
 
-            <EmergencyModal 
-                isOpen={showEmergencyModal}
-                onClose={() => setShowEmergencyModal(false)}
-            />
-
             <section className="flex flex-col flex-1 overflow-hidden px-12 py-8 pb-0">
                 <header className="flex items-center justify-between mb-8 shrink-0">
                     <div>
@@ -175,7 +168,7 @@ export default function TelaInicialPaciente() {
                             Bom dia, {nomeUsuario}
                         </h1>
                     </div>
-                    <EmergencyButton onClick={() => setShowEmergencyModal(true)} />
+                    <EmergencyButton onClick={() => navigate('/emergencia')} />
                 </header>
 
                 {loading ? (
